97c07a98e90986508ede9298b90c4c19922f6e31
alysson <absalysson@hotmail.com>
2022-11-08 22:09:35 -0300

    primeiro arquivo alocado dentro do git

---
+++ IMPV2.py
@@ -0,0 +1,28 @@
+#Sistema de Impressão Cambio
+
+
+import PyPDF2
+from PyPDF2 import PdfFileMerger, PdfFileReader, PageRange
+
+
+pdfFileObj = open ('Z:\Importacao\TRADINGS - TOUQUET E LICKY\GERENCIAL\REPORTS FP&A\Fechamento de Cambio\CAMBIO MARÇO.pdf', 'rb')
+print(pdfFileObj)
+pdfReader = PyPDF2.PdfFileReader (pdfFileObj)
+imprimir = (pdfReader.numPages)
+print(imprimir)
+pageObj = pdfReader.getPage(1)
+
+
+# extrai texto do objeto
+##imprimir2 = (pageObj.extractText())
+##print(imprimir2)
+
+
+#salvamento
+pdfWriter = PyPDF2.PdfFileWriter()
+pdfWriter.addPage(pageObj)
+newFileName = 'COTAÇÃO ITAU.pdf'
+newFile = open ('Z:\Importacao\TRADINGS - TOUQUET E LICKY\GERENCIAL\REPORTS FP&A\Fechamento de Cambio'+newFileName,'wb')
+pdfWriter.write (newFile)
+pdfFileObj.close()
+newFile.close()
