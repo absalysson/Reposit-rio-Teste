d657c06402de04fe7811911ea796a9e86c19876b
alysson <absalysson@hotmail.com>
2022-11-10 11:06:07 -0300

    Adicionamos uma nova função ao comando com o python

--- shebang.py
+++ shebang.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
-
+import os
+def check_reboot()
+"""returns true if teh com,puter has a pending reboot"""
+return os.path.exist("/run/reboot-required")
 def main():
     pass
 
