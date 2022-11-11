299c2be780c03950a2ad7fb71ffcef7b39fc5700
alysson <absalysson@hotmail.com>
2022-11-10 22:02:18 -0300

    Retorna mensagem de erro caso tenha reinicializao pendente

--- shebang.py
+++ shebang.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import os
+import sys
 def check_reboot()
 """returns true if teh com,puter has a pending reboot"""
+
+
+
+
 return os.path.exist("/run/reboot-required")
+
+
+
+
+
 def main():
-    pass
+    if check_reboot():
+        print("Pending Reboot")
+        sys.exit(l)
 
 main()
 
