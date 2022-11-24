82fb82ca4326b23734d23f7b948dca25dcd37405
alysson <absalysson@hotmail.com>
2022-11-24 09:12:02 -0300

    MUDANAÇA NO NOME DO ARQUIVO SHEBANG.PY PARA ALLCHEKS.PY

---
+++ allchecks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+
+def check_reboot():
+
+#returns true if the  computer has a pending reboot
+#adicionando comentario
+
+return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot")
+        sys.exit(1)
+    print("Everything ok")
+    sys.exit(0)
+
+main()
+
